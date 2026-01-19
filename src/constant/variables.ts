bef95405a8256544b510d06f1ca09fc21fe9c71c
Grisela Diki <griseladiki@gmail.com>
2026-01-19 21:13:30 +0700

    chore: configure rtk

---
+++ src/constant/variables.ts
@@ -0,0 +1,3 @@
+export const unsetLink = { textDecoration: "none", color: "unset" };
+export const APP_VERSION = import.meta.env.VITE_APP_VERSION;
+export const BASE_GRAPHQL_URL = import.meta.env.VITE_BASE_GRAPHQL_URL;
