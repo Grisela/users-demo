9fce3dbcebae91c8c9d1a0576c5cbdc65a7f4943
Grisela Diki <griseladiki@gmail.com>
2026-01-19 20:55:38 +0700

    chore: configure mantine

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import "@mantine/core/styles.css";
+import { createTheme, MantineProvider } from "@mantine/core";
 
-createRoot(document.getElementById('root')!).render(
+const theme = createTheme({
+  /**
+   * TODO theming
+   */
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </StrictMode>,
-)
+);
